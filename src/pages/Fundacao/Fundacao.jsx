95fd9ea58d075282eb3f042787f081e81a73cb4e
Iag0ow <iagomacedo159@gmail.com>
2023-05-24 22:10:14 -0300

    Alteração de imagens e loading incrementado

--- src/pages/Fundacao/Fundacao.jsx
+++ src/pages/Fundacao/Fundacao.jsx
@@ -13,9 +13,13 @@ const Fundacao = () => {
   const [price, setPrice] = useState("");
   const [brand, setBrand] = useState("");
   const [updated, setUpdated] = useState("");
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     async function fetchData() {
       setProducts(await getProducts("STRUCTURAL"));
+      setLoading(false);
+
     }
     fetchData();
   }, [updated]);
@@ -54,35 +58,49 @@ const Fundacao = () => {
                 </tr>
               </thead>
               <tbody>
-                {products &&
-                  products.map((product, index) => (
-                    <tr key={index}>
-                      <td>{product.name}</td>
-                      <td>{product.price}</td>
-                      <td>{product.brand}</td>
-                      <td>
-                        <div
-                          onClick={() => editItens(product._id)}
-                          className="pointer"
-                          type="button"
-                          data-bs-toggle="modal"
-                          data-bs-target="#exampleModal3"
-                          data-bs-whatever="@mdo"
-                        >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="currentColor"
-                            className="bi bi-pencil-fill"
-                            viewBox="0 0 16 16"
-                          >
-                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
-                          </svg>
+                {loading ? (
+                  <tr>
+                    <td colSpan="4">
+                      <div className="d-flex justify-content-center align-items-center">
+                        <div className="spinner-border" role="status">
+                          <span className="visually-hidden">Loading...</span>
                         </div>
-                      </td>
-                    </tr>
-                  ))}
+                      </div>
+                    </td>
+                  </tr>
+                ) : (
+                  <>
+                    {products &&
+                      products.map((product, index) => (
+                        <tr key={index}>
+                          <td>{product.name}</td>
+                          <td>{product.price}</td>
+                          <td>{product.brand}</td>
+                          <td>
+                            <div
+                              onClick={() => editItens(product._id)}
+                              className="pointer"
+                              type="button"
+                              data-bs-toggle="modal"
+                              data-bs-target="#exampleModal5"
+                              data-bs-whatever="@mdo"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+                                className="bi bi-pencil-fill"
+                                viewBox="0 0 16 16"
+                              >
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                              </svg>
+                            </div>
+                          </td>
+                        </tr>
+                      ))}
+                  </>
+                )}
               </tbody>
             </table>
           </div>
--- src/pages/Opçoes/Opcoes.jsx
+++ src/pages/Opçoes/Opcoes.jsx
@@ -30,30 +30,35 @@ const Opcoes = (handleFunc) => {
         <div className="container">
           <h1 className="text-center mt-5 title-orc">Opções de Gestão</h1>
           <div className="row mt-5">
-            <div className="col-md-4">
-              <Link to="/criar">
-                <div className="category hidraulica"></div>
-              </Link>
-              <h1 className="text-center categories mt-3">Criar Orçamento</h1>
-            </div>
-            <div className="col-md-4">
-              {avaliable ? (
-                <Link to={`/orcamentosEdit/${id}/budget/${idBudget}`}>
-                  <div className="category eletrica"></div>
+            <div className="opcoes">
+              <div className="col-md-4">
+                <Link to="/criar">
+                  <div className="category2 hidraulica2"></div>
                 </Link>
-              ) : (
-                <Link to={`/orcamentos`}>
-                  <div className="category eletrica"></div>
-                </Link>
-              )}
-              <h1 className="text-center categories mt-3">Editar Orçamentos</h1>
+                <h1 className="text-center categories mt-3">Criar Orçamento</h1>
+              </div>
+              <div className="col-md-4">
+                {avaliable ? (
+                  <Link to={`/orcamentosEdit/${id}/budget/${idBudget}`}>
+                    <div className="category2 eletrica2"></div>
+                  </Link>
+                ) : (
+                  <Link to={`/orcamentos`}>
+                    <div className="category2 eletrica2"></div>
+                  </Link>
+                )}
+                <h1 className="text-center categories mt-3">
+                  Editar Orçamentos
+                </h1>
+              </div>
             </div>
-            <div className="col-md-4">
+
+            {/* <div className="col-md-4">
               <Link to={`fundacao/STRUCTURAL`}>
-                <div className="category fundacao"></div>
+                <div className="category2 fundacao2"></div>
               </Link>
               <h1 className="text-center categories mt-3">Gerar PDF</h1>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
